c3db933e45fa034e265f7950a67ac247214ef9b8
David <fatokundavid2@gmail.com>
2023-06-11 18:40:18 -0600

    complete iss spotter with promises

---
+++ iss_promised.js
@@ -0,0 +1,43 @@
+const request = require('request-promise-native');
+
+/*
+ * Makes a request to ipwho.is using the provided IP address to get its geographical information (latitude/longitude)
+ * Input: JSON string containing the IP address
+ * Returns: Promise of request for lat/lon
+ */
+
+const fetchMyIP = function() {
+    return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+    const ip = JSON.parse(body).ip;
+    return request(`http://ipwho.is/${ip}`);
+};
+
+/*
+ * Requests data from https://iss-flyover.herokuapp.com using provided lat/long data
+ * Input: JSON body containing geo data response from ipwho.is
+ * Returns: Promise of request for fly over data, returned as JSON string
+ */
+const fetchISSFlyOverTimes = function(body) {
+    const { latitude, longitude } = JSON.parse(body);
+    const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+    return request(url);
+};
+
+/*
+ * Input: None
+ * Returns: Promise for fly over data for users location
+ */
+const nextISSTimesForMyLocation = function() {
+    return fetchMyIP()
+        .then(fetchCoordsByIP)
+        .then(fetchISSFlyOverTimes)
+        .then((data) => {
+            const { response } = JSON.parse(data);
+            return response;
+        });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
